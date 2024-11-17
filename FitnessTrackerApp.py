cb6dfedff7b08adb679e2d4c03ffe0cc801bf102
Snega Sri A <snegasri29052005@gmail.com>
2024-11-17 13:54:01 +0530

    Initial commit - Add Fitness Tracker App

--- FitnessTrackerApp.py
+++ FitnessTrackerApp.py
@@ -53,57 +53,87 @@ def fetch_user_profile_from_firebase(user_id):
     user_profile_ref = db.collection("users").document(user_id)
     user_profile = user_profile_ref.get().to_dict()
     return user_profile
+from datetime import datetime
+
+def save_feedback_to_firebase(feedback, email):
+    """Save user feedback to Firebase Firestore."""
+    feedback_entry = {
+        "feedback": feedback,
+        "email": email,
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    try:
+        db.collection("feedback").add(feedback_entry)  # Add the feedback to the feedback collection
+        return True
+    except Exception as e:
+        print(f"Error saving feedback: {e}")
+        return False
 
 
 # Function for user authentication
 def user_authentication():
-     # Add image and title
-    st.image("Fitness.png", width=200)
-    st.title("Health Tracker")
+    # Add image and title in the center of the page
+    col1, col2, col3 = st.columns([1, 2, 1])  # Create three columns
+    with col2:
+        st.image("lg2.png", width=300)
+        st.title("Fitness Tracker")
 
-    st.header("User Authentication")
-    
-    # Ask the user if they are new users or already have an account
-    user_type = st.radio("Are you a new user or do you already have an account?", ("New User", "Existing User"))
+    # Center the radio buttons (New User / Existing User)
+    col1, col2, col3 = st.columns([1, 2, 1])  # Create three columns for centering
+    with col2:
+        user_type = st.radio("Are you a new user or do you already have an account?", ("New User", "Existing User"))
 
+    # Existing User form in center layout
     if user_type == "Existing User":
-        # If the user is an existing user, prompt for email and password
-        email = st.text_input("Email")
-        password = st.text_input("Password", type="password")
-        if st.button("Sign In"):
-            try:
-                user = auth.get_user_by_email(email)
-                st.success(f"Welcome back, {user.email}!")
-                user_id = user.uid
-                st.session_state.user_id = user_id
-                st.experimental_rerun()
-            except auth.UserNotFoundError:
-                st.error("User not found. Please check your credentials or sign up.")
-            except Exception as e:
-                st.error(f"Error during sign-in: {e}")
-    else:
-        # If the user is a new user, prompt for account creation details
-        email = st.text_input("Email")
-        password = st.text_input("Password", type="password")
-        password_confirmation = st.text_input("Confirm Password", type="password")
-        name = st.text_input("Name")
-        age = st.number_input("Age", min_value=0, max_value=150)
-        weight = st.number_input("Weight (kg)", min_value=0)
-        daily_calorie_goal = st.number_input("Daily Calorie Goal", min_value=0)
-
-        if st.button("Sign Up"):
-            if password != password_confirmation:
-                st.error("Passwords do not match")
-            else:
+        col1, col2, col3 = st.columns([1, 2, 1])  # Create three columns for centering the form
+        with col2:
+            email = st.text_input("Email")
+            password = st.text_input("Password", type="password")
+            if st.button("Sign In"):
                 try:
-                    user = auth.create_user(email=email, password=password)
-                    st.success(f"User created: {user.email}")
+                    user = auth.get_user_by_email(email)
+                    st.success(f"Welcome back, {user.email}!")
                     user_id = user.uid
                     st.session_state.user_id = user_id
-                    save_user_profile_to_firebase(user_id, name, age, weight, daily_calorie_goal)
-                    st.experimental_rerun()
+                    st.rerun()
+
+                except auth.UserNotFoundError:
+                    st.error("User not found. Please check your credentials or sign up.")
                 except Exception as e:
-                    st.error(f"Error during sign-up: {e}")
+                    st.error(f"Error during sign-in: {e}")
+
+    # New User form in center layout
+    else:
+        col1, col2, col3 = st.columns([1, 2, 1])  # Create three columns for centering the form
+        with col2:
+            email = st.text_input("Email")
+            password = st.text_input("Password", type="password")
+            password_confirmation = st.text_input("Confirm Password", type="password")
+            name, age = st.columns(2)  # Name and Age in one line
+            with name:
+                name = st.text_input("Name")
+            with age:
+                age = st.number_input("Age", min_value=0, max_value=150)
+            weight, daily_calorie_goal = st.columns(2)  # Weight and Calorie Goal in one line
+            with weight:
+                weight = st.number_input("Weight (kg)", min_value=0)
+            with daily_calorie_goal:
+                daily_calorie_goal = st.number_input("Daily Calorie Goal", min_value=0)
+
+            if st.button("Sign Up"):
+                if password != password_confirmation:
+                    st.error("Passwords do not match")
+                else:
+                    try:
+                        user = auth.create_user(email=email, password=password)
+                        st.success(f"User created: {user.email}")
+                        user_id = user.uid
+                        st.session_state.user_id = user_id
+                        save_user_profile_to_firebase(user_id, name, age, weight, daily_calorie_goal)
+                        st.rerun()
+
+                    except Exception as e:
+                        st.error(f"Error during sign-up: {e}") 
 
 # Function to allow users to update their profile information
 def update_user_profile(user_id):
@@ -124,7 +154,8 @@ def update_user_profile(user_id):
             try:
                 save_user_profile_to_firebase(user_id, user_profile.get('Name', ''), new_age, new_weight, new_daily_calorie_goal)
                 st.success("Profile updated successfully")
-                st.experimental_rerun()
+                st.rerun()
+
             except Exception as e:
                 st.error(f"Error updating profile: {e}")
     else:
@@ -310,37 +341,24 @@ def fetch_monthly_summary(user_id):
             monthly_summary[month] = log_data["Calories"]
     return monthly_summary
 
-# Function to fetch monthly summary
-def fetch_monthly_summary(user_id):
-    food_logs = db.collection("users").document(user_id).collection("food_logs").stream()
-    monthly_summary = {}
-    for log in food_logs:
-        log_data = log.to_dict()
-        log_date = log_data["Date"].split(" ")[0]
-        month = datetime.strptime(log_date, "%Y-%m-%d").strftime("%Y-%m")
-        if month in monthly_summary:
-            monthly_summary[month] += log_data["Calories"]
-        else:
-            monthly_summary[month] = log_data["Calories"]
-    return monthly_summary
-
+# Main Streamlit app
 # Main Streamlit app
 def main():
     if "user_id" not in st.session_state:
         user_authentication()
         return
 
-    st.set_page_config(page_title="Health Tracker", layout="wide")
-    st.title("Health Tracker")
+    st.set_page_config(page_title="Fitness Tracker", layout="wide")
+    st.title("Fitness Tracker")
 
     user_id = st.session_state.user_id
 
     user_profile = fetch_user_profile_from_firebase(user_id)
     if user_profile:
-        st.write(f"**Name:** {user_profile.get('Name', '')}")
-        st.write(f"**Age:** {user_profile.get('Age', '')}")
-        st.write(f"**Weight:** {user_profile.get('Weight', '')} kg")
-        st.write(f"**Daily Calorie Goal:** {user_profile.get('Daily Calorie Goal', '')} calories")
+        st.write(f"*Name:* {user_profile.get('Name', '')}")
+        st.write(f"*Age:* {user_profile.get('Age', '')}")
+        st.write(f"*Weight:* {user_profile.get('Weight', '')} kg")
+        st.write(f"*Daily Calorie Goal:* {user_profile.get('Daily Calorie Goal', '')} calories")
 
     update_user_profile(user_id)
     log_exercise(user_id)
@@ -351,7 +369,8 @@ def main():
     display_water_intake_statistics(user_id)
     display_sleep_statistics(user_id)
 
-     # Display calorie goal and progress
+ 
+      # Display calorie goal and progress
     st.header("Calorie Goal Progress")
     daily_summary = fetch_daily_summary(user_id)
     daily_summary_df = pd.DataFrame(list(daily_summary.items()), columns=["Date", "Calories"])
@@ -360,59 +379,56 @@ def main():
         total_calories_consumed = daily_summary_df["Calories"].sum()
         progress_percentage = min(100, int((total_calories_consumed / daily_calorie_goal) * 100))
         
-        st.write(f"**Daily Calorie Goal:** {daily_calorie_goal} calories")
-        st.write(f"**Total Calories Consumed:** {total_calories_consumed} calories")
-        st.write(f"**Progress:** {progress_percentage}%")
+        st.write(f"*Daily Calorie Goal:* {daily_calorie_goal} calories")
+        st.write(f"*Total Calories Consumed:* {total_calories_consumed} calories")
+        st.write(f"*Progress:* {progress_percentage}%")
         
         # Display progress bar
         st.progress(progress_percentage / 100)
-
 # Sidebar
     st.sidebar.title("Welcome to Fitness Tracker")
-    st.sidebar.image("Fitness.png", use_column_width=True)
+    st.sidebar.image("lg2.png", use_container_width=True)
 
     st.sidebar.header("About")
-    st.sidebar.write("Welcome to Fitness Tracker, your personal assistant assistant!")
-    st.sidebar.write("Track your daily activities and stay on top of your health goals.")
+    st.sidebar.write("Welcome to Fitness Tracker, your personal fitness companion!")
+    st.sidebar.write("Track your workouts, monitor your progress, and achieve your fitness goals with ease.")
+    st.sidebar.write("We provide a user-friendly interface for logging food intake, workouts, water intake, and sleep. You can also track your progress and set fitness goals.")
 
     st.sidebar.subheader("Features")
-    st.sidebar.write("- Log food intake, exercise, water intake, and sleep")
-    st.sidebar.write("- View detailed statistics and visualizations")
-    st.sidebar.write("- Set and track daily calorie goal progress")
-    st.sidebar.write("- User-friendly interface")
+    st.sidebar.write("- Log workouts, water intake, and sleep")
+    st.sidebar.write("- View personalized fitness statistics and insights")
+    st.sidebar.write("- Set and track your fitness goals")
+    st.sidebar.write("- Access a user-friendly and responsive interface")
+    st.sidebar.write("- Nutrition tracking using the Nutritionix API for detailed food logs and calorie tracking")
 
 # Improved call to action with icon and link
-    st.sidebar.subheader(" Want to level up your workouts?")
-    st.sidebar.write("We've created a gym workout app that curates a personalized workout plan for you!")
+    st.sidebar.subheader("Want to level up your fitness journey?")
+    st.sidebar.write("Discover our advanced gym workout planner designed to help you maximize your results!")
     link = "https://musclemate-app.netlify.app/"
-    st.sidebar.write(f"Check out MuscleMate: {link}")
+    st.sidebar.write(f"Check out MuscleMate: [Click here]({link})")
 
-    st.sidebar.subheader("Technology Stack")
-
+    st.sidebar.subheader("API Integration: Nutritionix API")
     st.sidebar.markdown("""
-    - **Frontend:** Streamlit - A Python library for creating interactive web applications quickly and easily. Streamlit allows for a user-friendly interface with minimal coding effort.
-    - **Backend:** Firebase - Google's cloud-hosted platform for mobile and web app development. Firebase provides functionalities like user authentication, data storage with a real-time database, and secure backend services.
-    - **API Integration:** Nutritionix API - This API allows the Health Tracker app to retrieve nutritional information for food items entered by users. This simplifies calorie tracking and empowers users to make informed dietary decisions.
+    - *Frontend:* Streamlit - A Python library for creating interactive web applications quickly and easily. Streamlit allows for a user-friendly interface with minimal coding effort.
+    - *Backend:* Firebase - Google's cloud-hosted platform for mobile and web app development. Firebase provides functionalities like user authentication, data storage with a real-time database, and secure backend services.
+    - *API Integration:* Nutritionix API - This API allows the Health Tracker app to retrieve nutritional information for food items entered by users. This simplifies calorie tracking and empowers users to make informed dietary decisions.
     """)
-
     st.sidebar.subheader("Architecture and Design")
 
     st.sidebar.markdown("""
-    The Health Tracker application leverages a Service-Oriented Architecture (SOA) to promote scalability and reusability. In SOA, functionalities are encapsulated as independent services that communicate with each other through well-defined interfaces (APIs). This project implements a simple SOA with two key components:
+    The Fitness Tracker application leverages a Service-Oriented Architecture (SOA) to promote scalability and reusability. In SOA, functionalities are encapsulated as independent services that communicate with each other through well-defined interfaces (APIs). This project implements a simple SOA with two key components:
+    - *Publisher App:* This app (in this case, the Nutritionix API) exposes services through APIs. The Health Tracker application consumes these APIs to access specific functionalities. For example, the Nutritionix API provides an API for retrieving calorie information based on a food item search.
+    - *Consumer App:* This is your Health Tracker application. It utilizes APIs exposed by the publisher app to deliver its features. By consuming the Nutritionix API, the Health Tracker empowers users to track their calorie intake seamlessly.
+    *Benefits of using SOA:*
 
-    - **Publisher App:** This app (in this case, the Nutritionix API) exposes services through APIs. The Health Tracker application consumes these APIs to access specific functionalities. For example, the Nutritionix API provides an API for retrieving calorie information based on a food item search.
-    - **Consumer App:** This is your Health Tracker application. It utilizes APIs exposed by the publisher app to deliver its features. By consuming the Nutritionix API, the Health Tracker empowers users to track their calorie intake seamlessly.
-
-    **Benefits of using SOA:**
-
-    - **Scalability:** SOA allows for independent scaling of publisher and consumer apps. If the user base for Health Tracker grows significantly, the backend infrastructure can be scaled without affecting the Nutritionix API.
-    - **Reusability:** APIs exposed by the publisher app can be consumed by various consumer applications. This promotes code reuse and reduces development time for future apps requiring similar functionalities.
-    - **Loose Coupling:** Consumer apps are not tightly coupled to the implementation details of the publisher app. This allows for flexibility and easier maintenance in the future.
+    - *Scalability:* SOA allows for independent scaling of publisher and consumer apps. If the user base for Health Tracker grows significantly, the backend infrastructure can be scaled without affecting the Nutritionix API.
+    - *Reusability:* APIs exposed by the publisher app can be consumed by various consumer applications. This promotes code reuse and reduces development time for future apps requiring similar functionalities.
+    - *Loose Coupling:* Consumer apps are not tightly coupled to the implementation details of the publisher app. This allows for flexibility and easier maintenance in the future.
     """)
 
     st.sidebar.subheader("We value your feedback!")
-    st.sidebar.write("Please share your thoughts to help us improve our app.")
-
+    st.sidebar.write("Your insights help us improve Fitness Tracker to better serve your fitness needs.")
+    
     feedback_paragraph = st.sidebar.text_area("Your feedback", height=150, placeholder="Enter your feedback here...")
     user_email = st.sidebar.text_input("Your email", placeholder="Enter your email here...")
 
@@ -422,13 +438,19 @@ def main():
         elif not user_email.strip():
             st.sidebar.error("Email cannot be empty. Please enter your email.")
         else:
-            # Add your backend handling code here, e.g., sending the feedback to your backend or storing it in a database
-            st.sidebar.success("Thank you for your feedback! It has been submitted successfully.")
-            # Optionally, clear the inputs after submission
-            st.session_state["feedback_paragraph"] = ""
-            st.session_state["user_email"] = ""
-            st.experimental_rerun()
+            success = save_feedback_to_firebase(feedback_paragraph.strip(), user_email.strip())
+            if success:
+                st.sidebar.success("Thank you for your feedback! It has been submitted successfully.")
+            # Clear inputs after submission
+                st.session_state["feedback_paragraph"] = ""
+                st.session_state["user_email"] = ""
+                st.rerun()
+
+            else:
+                st.sidebar.error("There was an error submitting your feedback. Please try again.")
+
             
+          
     st.title("Calorie Tracker")
     st.header("Add a new food log")
     option = st.radio("How do you want to add your log?", ("Manually", " Automatically (Using Nutritionix API)"))
@@ -455,6 +477,7 @@ def main():
             else:
                 st.error("Please enter a food item")
 
+
 # Modify the section for displaying food logs and deletion logic
     st.header("Food Logs")
     food_logs = db.collection("users").document(user_id).collection("food_logs").stream()
@@ -469,13 +492,14 @@ def main():
             if st.button("Delete Log"):
                 delete_log_from_firebase(log_to_delete, user_id)
                 st.success("Log deleted successfully")
-                st.experimental_rerun()
+                st.rerun()
 
-        if st.button("Clear All Logs"):
+    if st.button("Clear All Logs"):
             if st.checkbox("Confirm Clear All Logs"):
                 clear_logs_from_firebase(user_id)
                 st.success("All logs cleared successfully")
-                st.experimental_rerun()
+                st.rerun()
+
     else:
         st.write("No logs available")
 
@@ -495,7 +519,6 @@ def main():
 
     weekly_summary = fetch_weekly_summary(user_id)
     monthly_summary = fetch_monthly_summary(user_id)
-
     if weekly_summary:
         weekly_summary_df = pd.DataFrame(list(weekly_summary.items()), columns=["Week", "Calories"])
         st.subheader("Weekly Calorie Consumption")
@@ -508,7 +531,8 @@ def main():
 
     if st.button("Logout", key="logout_button"):
         del st.session_state["user_id"]
-        st.experimental_rerun()
+        st.rerun()
+
 
 if __name__ == "__main__":
     main()
